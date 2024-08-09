98882d137446d06c810a7819860aa901be923ea0
Alausa Aishat <qtaisha.ao@gmail.com>
2024-08-09 12:13:48 +0100

    building medik.ai

---
+++ backend-api/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import tensorflow as tf
+import numpy as np
+
+app = Flask(__name__)
+CORS(app) #to enable CORS for all routes
+
+#load trained model
+model = tf.keras.models.load_model('model/')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    #get data from POST
+    data = request.json
+    
+    #preprocess input data
+    input_data = preprocess(data)
+
+    #run prediction
+    prediction = model.predict(input_data)
+
+    #process the prediction to return to user
+    result = postprocess(prediction)
+
+    return jsonify(result)
+
